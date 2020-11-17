65872c16e1111e81a8b0accdbdd7ac9043d16e7d
JE-Chen <zenmailman@gmail.com>
2020-11-17 13:16:22 +0800

    refactor bot.py
    refactor bot.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -1,11 +1,12 @@
 # bot.py
-import sys
 import configparser
-from discord.ext import commands
 import datetime
-import discord
+import sys
 
-config= configparser.ConfigParser()
+import discord
+from discord.ext import commands
+
+config = configparser.ConfigParser()
 config.read('Token.ini')
 TOKEN = (config.get('Token', 'Token'))
 
@@ -13,11 +14,11 @@ try:
     bot = commands.Bot(command_prefix='JE-')
     bot.remove_command("help")
 
+
     @bot.command(pass_content=True)
     async def Ping(ctx):
-        server_name = ctx.message.guild.name
-        server_id=ctx.message.guild.id
         await ctx.message.channel.send("Pong")
+
 
     @bot.command(pass_content=True)
     async def clear(ctx, amount=100):
@@ -25,8 +26,9 @@ try:
 
 
     @bot.command(pass_content=True)
-    async def close(ctx,rolename='Bot'):
+    async def close():
         sys.exit()
+
 
     @bot.command(pass_content=True)
     async def login_out():
@@ -35,27 +37,29 @@ try:
 
     @bot.event
     async def on_ready():
-        print(f'{bot.user.name} has connected to Discord!','TAG : Bot connect')
-        activity = discord.Game(name="JE-",type=discord.ActivityType.streaming)
-        await bot.change_presence(status=discord.Status.idle, activity=activity)
+        print(f'{bot.user.name} has connected to Discord!', 'TAG : Bot connect')
+        activity = discord.Game(name="JE-", type=discord.ActivityType.streaming)
+        await bot.change_presence(status=discord.Status.online, activity=activity)
+
 
     @bot.event
-    async def on_reaction_add(reaction,user):
-
-        channel=reaction.message.channel
-
-        print(user.name, ' has  add ', reaction.emoji, ' to ', reaction.message.content,'TAG : reaction')
-
-        await channel.send("{} has added {} to the message {}".format(user.name,reaction.emoji,reaction.message.content))
-
-    @bot.event
-    async def on_reaction_remove(reaction,user):
+    async def on_reaction_add(reaction, user):
 
         channel = reaction.message.channel
 
-        print(user.name,' has  removed ',reaction.emoji,' to ',reaction.message.content,'TAG : reaction')
+        print(user.name, ' has  add ', reaction.emoji, ' to ', reaction.message.content, 'TAG : reaction')
 
-        await channel.send("{} has removed {} to the message {}".format(user.name, reaction.emoji,reaction.message.content))
+        await channel.send(
+            "{} has added {} to the message {}".format(user.name, reaction.emoji, reaction.message.content))
+
+
+    @bot.event
+    async def on_reaction_remove(reaction, user):
+
+        channel = reaction.message.channel
+        print(user.name, ' has  removed ', reaction.emoji, ' to ', reaction.message.content, 'TAG : reaction')
+        await channel.send(
+            "{} has removed {} to the message {}".format(user.name, reaction.emoji, reaction.message.content))
 
 
     @bot.event
@@ -67,28 +71,26 @@ try:
             f'Hi {member.name}, welcome to  Discord server!'
         )
 
+
     @bot.event
     async def on_message(message):
 
         if message.author == bot.user:
             return
-
-        if message!=None:
-            print(datetime.datetime.now(),message.author,':', message.content,sep=' ')
-
-        #await message.channel.send(message.content)
+        if message != None:
+            print(datetime.datetime.now(), message.author, ':', message.content, sep=' ')
         await bot.process_commands(message)
+
 
     @bot.event
     async def on_message_delete(message):
 
-
         author = message.author
         content = message.content
-        '''
-        if message.author !=bot.user:
+        if message.author != bot.user:
+            print(f"{author} Delete the message {content}")
             await message.channel.send("{} Delete the message {}".format(author, content))
-        '''
+
 
     @bot.event
     async def on_error(event, *args, **kwargs):
@@ -102,4 +104,4 @@ except Exception as Print_Exception:
     print(Print_Exception)
 
 finally:
-    bot.run(TOKEN,bot=True)
+    bot.run(TOKEN, bot=True)
